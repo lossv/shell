b78a170e8df65d951f3ffd05994d60c896308b19
OuYanghaoyue <39338698+OuYanghaoyue@users.noreply.github.com>
2019-03-06 21:57:26 +0800

    Add files via upload

---
+++ install_python3.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/
+
+
+wget https://www.python.org/ftp/python/3.7.2/Python-3.7.2.tgz
+tar -zxf Python-3.7.2.tgz
+cd Python-3.7.2
+mkdir /usr/python3
+./configure --prefix=/usr/python3
+make && make install
+ln -s /usr/python3/bin/python3.7 /usr/bin/python3
+ln -s /usr/python3/bin/pip3.7 /usr/bin/pip3
